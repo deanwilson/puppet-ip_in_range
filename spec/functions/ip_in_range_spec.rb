b384ea19197a3391b34da04f9706ed3bad571bba
Dean Wilson <dean.wilson@gmail.com>
2018-08-18 09:21:13 +0100

    Move to the new exceptional handling style of spec
    Initially done to avoid deprecation warnings.
    
    This change also refines the exception checking slightly
    by ensuring the error contains some of the expected string.

--- spec/functions/ip_in_range_spec.rb
+++ spec/functions/ip_in_range_spec.rb
@@ -19,14 +19,14 @@ describe 'ip_in_range' do
   # Invalid function call tests
   #################################################
   it 'should throw an ArgumentError unless called with 2 arguments' do
-    expect { subject.call([1, 2, 3]) }.to raise_error(ArgumentError)
+    is_expected.to run.with_params().and_raise_error(ArgumentError, /Wrong number/)
   end
 
   it 'should throw an ArgumentError with an invalid ip address' do
-    expect { subject.call(['300.2.3.4', '192.168.100.0/24']) }.to raise_error(ArgumentError)
+    is_expected.to run.with_params('300.2.3.4', '192.168.100.0/24').and_raise_error(ArgumentError, /invalid ip address/)
   end
 
-  it 'Should throw an ArgumentError with an invalid ip range' do
-    expect { subject.call(['192.168.100.12', '192.168.100.0/242']) }.to raise_error(ArgumentError)
+  it 'Should throw an ArgumentError with an invalid CIDR range' do
+    is_expected.to run.with_params('192.168.100.12', '192.168.100.0/242').and_raise_error(ArgumentError, /invalid ip address/)
   end
 end
