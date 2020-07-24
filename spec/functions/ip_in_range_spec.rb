e6c777f7c17f1a9ccd713ba170e88ada58236086
Dean Wilson <dean.wilson@gmail.com>
2020-07-24 08:27:08 +0100

    Fix RSpec/ExampleWording by editing the `it` strings
    This does make the descriptions a little nicer.

--- spec/functions/ip_in_range_spec.rb
+++ spec/functions/ip_in_range_spec.rb
@@ -3,30 +3,30 @@ require 'spec_helper'
 describe 'ip_in_range' do
   # Basic functionality tests
   #################################################
-  it 'should return true when the ip is contained in the range' do
+  it 'returns true when the ip is contained in the range' do
     should run.with_params('192.168.100.12', '192.168.100.0/24').and_return(true)
   end
 
-  it 'should return true when the ip is contained in an array of ranges' do
+  it 'returns true when the ip is contained in an array of ranges' do
     ranges = ['10.10.10.10/24', '192.168.100.0/24']
     should run.with_params('192.168.100.12', ranges).and_return(true)
   end
 
-  it 'should return false when an ip is not in the range' do
+  it 'returns false when an ip is not in the range' do
     should run.with_params('10.10.10.10', '192.168.100.0/24').and_return(false)
   end
 
   # Invalid function call tests
   #################################################
-  it 'should throw an ArgumentError unless called with 2 arguments' do
+  it 'throws an ArgumentError unless called with 2 arguments' do
     is_expected.to run.with_params.and_raise_error(ArgumentError, /Wrong number/)
   end
 
-  it 'should throw an ArgumentError with an invalid ip address' do
+  it 'throws an ArgumentError when given an invalid ip address' do
     is_expected.to run.with_params('300.2.3.4', '192.168.100.0/24').and_raise_error(ArgumentError, /invalid ip/)
   end
 
-  it 'Should throw an ArgumentError with an invalid CIDR range' do
+  it 'throws an ArgumentError when given an invalid CIDR range' do
     is_expected.to run.with_params('192.168.100.12', '192.168.100.0/242').and_raise_error(ArgumentError, /invalid ip/)
   end
 end
